b0548eb7f567754f180f45007c1939fadf94bff6
ramona484 <ramona.rettig@outlook.de>
2021-02-05 05:38:14 +0100

    test linting

--- Dockerfile
+++ Dockerfile
@@ -6,3 +6,5 @@ RUN npm install
 
 EXPOSE 8080
 CMD [ "npm", "start" ]
+
+  AKUIS6VEP9M7KLD5UIO69
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('AKUIS6VEP9M7KLD5UIO69');
+  res.send('Hello World');
 });
 
 app.listen(PORT, HOST);
