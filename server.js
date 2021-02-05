50a8d01caad5a1ad9234bd5251924eded7dd8b05
ramona484 <ramona.rettig@outlook.de>
2021-02-05 04:47:44 +0100

    test linting

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('AKUIS6VEP9M7KLD5UIO69');
 });
 
 app.listen(PORT, HOST);
